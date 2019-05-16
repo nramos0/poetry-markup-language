cee6b25d96cd93a8f5c5d9c10d530f2560ef8ab2
nramos0 <nramos@imsa.edu>
2019-05-15 22:44:19 -0500

    main page viewer/editor buttons added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,86 @@
 
 window.onload = loadMainPage;
+const path = require('path');
+const url = require('url');
+const remote = require('electron').remote;
 
 function loadMainPage() {
     loadButtonListeners();
 }
 
 function loadButtonListeners() {
+    var editorButton = document.getElementById('editorButton');
+    var viewerButton = document.getElementById('viewerButton');
+
+    function callback() {
+        remote.getCurrentWindow().show();
+    }
+
+    editorButton.addEventListener('click', function(err) {
+        createWindow(800, 600, 'editor.html', callback);
+        remote.getCurrentWindow().hide();
+    });
+
+    viewerButton.addEventListener('click', function(err) {
+        createWindow(800, 600, 'viewer.html', callback);
+        remote.getCurrentWindow().hide();
+    });
+}
+
+function createWindow(widthInput, heightInput, htmlFilename, onClosedCallback, isVisible) {
+    // Constructs window
+    let win;
+    var options = {
+        width: widthInput, 
+        height: heightInput,
+        show: isVisible,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    };
+    try {
+        win = new BrowserWindow(options);
+    } 
+    catch (err) {
+        // If there is an error in creating a BrowserWindow, it is probably because 
+        // a BrowserWindow is being created from a renderer process, which is not
+        // allowed unless it uses Electron's remote
+        BrowserWindow = remote.BrowserWindow;
+        win = new BrowserWindow(options);
+    }
+
+    if (isVisible != false) {
+        win.once('ready-to-show', function() {
+            win.show();
+        });
+    }
     
+    // Loads initial html file
+    var htmlFilePath = path.join(__dirname, '../src/' + htmlFilename);
+
+    win.loadURL(url.format({
+      pathname: htmlFilePath,
+      protocol: 'file:',
+      slashes: true
+    }))
+  
+    // Prevents the calling of 'eval()' which allows any arbitrary
+    // JS code to be evaluted. This is for security purposes.
+    win.eval = global.eval = function () {
+      throw new Error('Sorry, this app does not support window.eval().')
+    }
+  
+    // Opens the Chrome page inspector when the window opens for easy debugging
+
+    // win.webContents.openDevTools();
+  
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        if (onClosedCallback !== undefined) {
+            onClosedCallback(win);
+        }
+        win = null;
+    }); 
+    return win;   
 }
